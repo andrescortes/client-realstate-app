9f6d0ec7f5db0e1ca4d84887749e2c2fda75f30e
Miguel Cortes <miguel.cortes@ias.com.co>
2022-10-11 08:27:51 -0500

    added structure project shared,degraded version firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client-realstate-app';
+
+  constructor(private fs: AngularFirestore) {
+  }
+
+  ngOnInit(): void {
+    this.fs.collection("test").stateChanges().subscribe({
+      next(persons) {
+        console.log(persons.map(x => x.payload.doc.data()))
+      }
+    })
+  }
+
+
 }
