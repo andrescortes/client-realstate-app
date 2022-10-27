9ae29617b3e70b45a10b2c03ef8783e86abfe9e5
Miguel Cortes <miguel.cortes@ias.com.co>
2022-10-27 09:00:59 -0500

    added module and exports at project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,9 +26,16 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {HeaderComponent} from './components/header/header.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import { MenuListComponent } from './components/menu-list/menu-list.component';
+import {MenuListComponent} from './components/menu-list/menu-list.component';
 import {MatListModule} from "@angular/material/list";
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {StoreModule} from '@ngrx/store';
+import {effects, reducers} from './store';
+import {EffectsModule} from '@ngrx/effects';
+import {HttpClientModule} from '@angular/common/http';
 
+
+const StoreDevtools = !environment.production ? StoreDevtoolsModule.instrument({maxAge: 50}) : [];
 
 @NgModule({
   declarations: [
@@ -61,6 +68,16 @@ import {MatListModule} from "@angular/material/list";
     MatButtonModule,
     FlexLayoutModule,
     MatListModule,
+    //store, reducers
+    StoreDevtools,
+    StoreModule.forRoot(reducers, {
+      runtimeChecks: {//persist to data on localstorage
+        strictActionImmutability: true,
+        strictStateImmutability: true
+      }
+    }),
+    EffectsModule.forRoot(effects),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
