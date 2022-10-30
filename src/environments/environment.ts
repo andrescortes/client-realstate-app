d5e60841a73f45a06e977eea7e4ce1f2c2f3ba00
Miguel Andres Cortes Reyes <andresabyac@gmail.com>
2022-10-29 21:35:03 -0500

    added file environment with credentials firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  url: 'http://localhost:5555/',
+  firebase: {
+    config: {
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
+    }
+  }
+};
