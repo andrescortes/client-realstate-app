42fe0e7955dd9931077397bc1be8d35708ce00d7
Miguel Cortes <miguel.cortes@ias.com.co>
2022-11-02 09:18:50 -0500

    moved files,new component upload photo to firebase

--- src/app/pages/real-state/real-state-routing.module.ts
+++ src/app/pages/real-state/real-state-routing.module.ts
@@ -5,12 +5,12 @@ import { AuthGuard } from '../../guards/auth/auth.guard';
 const routes: Routes = [
   {
     path: 'create',
-    loadChildren: () => import('./real-state-create/real-state-create.module').then(m => m.RealStateCreateModule),
+    loadChildren: () => import('./pages/real-state-create/real-state-create.module').then(m => m.RealStateCreateModule),
     canActivate: [AuthGuard]
   },
   {
     path: 'list',
-    loadChildren: () => import('./real-state-list/real-state-list-routing.module').then(m => m.RealStateListRoutingModule),
+    loadChildren: () => import('./pages/real-state-list/real-state-list-routing.module').then(m => m.RealStateListRoutingModule),
     canActivate: [AuthGuard]
   }
 ];
