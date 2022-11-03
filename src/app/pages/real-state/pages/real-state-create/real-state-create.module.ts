09774e3466ac10f657e898d73aa60832f2995d4d
Miguel Cortes <miguel.cortes@ias.com.co>
2022-11-03 08:58:18 -0500

    added form, styles and functionality upload images to firebase.

--- src/app/pages/real-state/pages/real-state-create/real-state-create.module.ts
+++ src/app/pages/real-state/pages/real-state-create/real-state-create.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { RealStateCreateRoutingModule } from './real-state-create-routing.module';
 import { RealStateCreateComponent } from './real-state-create.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SpinnerModule } from 'src/app/shared/indicators';
+import { EntityPhotoModule } from 'src/app/shared/layouts';
+import { PopupsModule } from 'src/app/shared/popups';
 
 
 @NgModule({
@@ -11,7 +22,20 @@ import { RealStateCreateComponent } from './real-state-create.component';
   ],
   imports: [
     CommonModule,
-    RealStateCreateRoutingModule
+    RealStateCreateRoutingModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule,
+    SpinnerModule,
+    EntityPhotoModule,
+    PopupsModule
   ]
 })
-export class RealStateCreateModule { }
+export class RealStateCreateModule {
+}
